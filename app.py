090e4d768c2149ebb9e989ce0417dee1df5522a0
Swaraj_Atkare <161420580+swarajatkare@users.noreply.github.com>
2025-02-18 18:09:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-import psycopg2 
-from flask import Flask 
-app = Flask(__name__) 
-def connect_db(): 
-  return psycopg2.connect( 
-    dbname="mydb", 
-    user="user", 
-    password="password", 
-    host="db" 
-) 
-@app.route("/") 
+import psycopg2
+from flask import Flask
+
+app = Flask(__name__)
+
+def connect_db():
+    return psycopg2.connect(
+        dbname="mydb",
+        user="user",
+        password="password",
+        host="db"
+    )
+
+@app.route("/")
 def home():
-  conn = connect_db() 
-  return "Connected to DB!" 
-if __name__ == "__main__": 
-app.run(host="0.0.0.0", port=6000) 
+    conn = connect_db()
+    return "Connected to DB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
